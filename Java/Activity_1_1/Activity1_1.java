decda8cb1959301bf6914902e4ad52d5af68bd11
sindhu516 <73517215+sindhu516@users.noreply.github.com>
2020-11-21 19:49:24 +0530

    Add files via upload

--- Java/Activity_1_1/Activity1_1.java
+++ Java/Activity_1_1/Activity1_1.java
@@ -1,1 +1,26 @@
+package JavaActivity1_1;
 
+public class Activity1 {
+	
+	public static void main(String[] args) {
+
+		Car Benz = new Car();
+
+		Benz.color = "black";
+
+		Benz.transmission = "Auto";
+
+		Benz.make = 0;
+
+		Benz.doors = 2;
+
+		Benz.tyres = 4;
+
+		Benz.displayCharacteristics();
+
+		Benz.accelerate();
+
+		Benz.brake();
+
+	}
+}
